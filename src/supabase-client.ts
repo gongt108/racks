472aa02a83337fd6625530b5263850af09e701bb
Tiffany Gong <76094291+gongt108@users.noreply.github.com>
2026-01-15 08:03:29 -0800

    Delete supabase-client.ts

--- src/supabase-client.ts
+++
@@ -1,9 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = import.meta.env.SUPABASE_PROJECT_URL;
-const supabasePublishableAPIKey = import.meta.env.SUPABASE_PUBLISHABLE_API_KEY;
-
-export const supabase = createClient(
-    supabaseUrl,
-    supabasePublishableAPIKey
-)
