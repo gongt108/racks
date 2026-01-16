53624a260aa018f3ed39f0e293aaf3b62d9f5f4e
Tiffany Gong <76094291+gongt108@users.noreply.github.com>
2026-01-16 00:33:26 -0800

    Update MainLayout.tsx

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,6 +1,6 @@
 // src/layouts/MainLayout.tsx
 import { Outlet } from "react-router-dom";
-// import Header from "../components/Header";
+import Header from "../components/Header";
 import Footer from "../components/Footer";
 
 const MainLayout = () => {
