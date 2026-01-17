310c93691b1b062ee8c6d6812684e31ebadedf69
Tiffany Gong <76094291+gongt108@users.noreply.github.com>
2026-01-16 18:27:35 -0800

    Move image upload inide bulk upload card

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,15 +3,15 @@ const Index = () => {
   return (
     <div className="min-h-screen bg-background">
       <main className="container mx-auto px-4 py-8">
-        <div>
-          <div className="flex flex-row sm:flex-col space-x-2 mx-auto p-4 border rounded-lg border-gray-500 hover:border-rose-600 transition-transform duration-200 hover:-translate-y-[2px]">
+        <div className="mx-auto p-4 border rounded-lg border-gray-500 hover:border-rose-600 transition-transform duration-200 hover:-translate-y-[2px]">
+          <div className="flex flex-row sm:flex-col space-x-2 mb-4">
             <div>Icon</div>
             <div className="flex flex-col space-between">
               <h1 className="text-xl font-bold">Bulk Upload</h1>
               <p className="text-md">Upload multiple items at once from your gallery</p>
             </div>
           </div>
-          <div className="rounded-lg bg-gray-500 flex">
+          <div className="rounded-lg bg-gray-200 border-dashed flex flex-col space-y-2">
             <div>icon</div>
             <h2 className="font-semibold text-lg">Select Photos</h2>
             <p>Choose multiple images</p>
