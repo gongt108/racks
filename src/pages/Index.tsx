25fbc30cc0af1e89019de46642e13717d8b577e5
Tiffany Gong <76094291+gongt108@users.noreply.github.com>
2026-01-17 21:25:31 -0800

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -5,9 +5,9 @@ const Index = () => {
   return (
     <div className="min-h-screen bg-background">
       <main className="container mx-auto px-4 py-8">
-        <div className="mx-auto py-4 px-8 w-full md:w-[56rem] border rounded-lg border-gray-500 hover:border-rose-600 transition-transform duration-200 hover:-translate-y-[2px]">
-          <div className="flex flex-col md:flex-row space-x-2">
-            <div className="bg-purple-600 p-3 rounded-md">
+        <div className="mx-auto py-8 px-8 w-full md:w-[56rem] border rounded-lg border-gray-500 hover:border-rose-600 transition-transform duration-200 hover:-translate-y-[2px]">
+          <div className="flex flex-col md:flex-row">
+            <div className="bg-purple-600 p-3 rounded-md md:mr-2">
               <ImageIcon className="w-6 h-6 text-white" />
             </div>
             <div className="flex flex-col space-between">
@@ -15,7 +15,7 @@ const Index = () => {
               <p className="text-md">Upload multiple items at once from your gallery</p>
             </div>
           </div>
-          <div className="rounded-lg bg-gray-200 border-dashed flex flex-col items-center text-center m-4 p-4 space-y-2">
+          <div className="rounded-lg bg-gray-200 border-dashed flex flex-col items-center text-center mx-4 my-6 p-4 space-y-2">
             <UploadIcon />
             <h2 className="font-semibold text-lg">Select Photos</h2>
             <p>Choose multiple images</p>
