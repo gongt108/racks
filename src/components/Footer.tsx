4209c9a63c9c6ce32d9a13e7752320f1526bb178
Tiffany Gong <gongt108@gmail.com>
2026-01-18 10:05:14 -0800

    refactor footer to use usenavigation

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,6 @@
 // src/components/Footer.tsx
-import { useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+
 import {
 	Add as AddIcon,
 	Inventory as InventoryIcon,
@@ -9,29 +10,46 @@ import {
 	Settings as SettingsIcon,
 } from '@mui/icons-material';
 
-const tabs = [
-	{ id: 'add', label: 'Add Item', Icon: AddIcon },
-	{ id: 'inventory', label: 'Inventory', Icon: InventoryIcon },
-	{ id: 'sold', label: 'Sold Items', Icon: AttachMoneyIcon },
-	{ id: 'dashboard', label: 'Dashboard', Icon: TrendingUpIcon },
-	{ id: 'till', label: 'Till', Icon: PointOfSaleIcon },
-	{ id: 'settings', label: 'Settings', Icon: SettingsIcon },
-];
-
 const Footer = () => {
-	const [activeTab, setActiveTab] = useState<string>('add');
+	const navigate = useNavigate();
+	const { pathname } = useLocation();
+
+	const activeTab = pathname.split('/')[1] || 'add';
+
+	const tabs = [
+		{ id: 'add', label: 'Add Item', path: '/', Icon: AddIcon },
+		{
+			id: 'inventory',
+			label: 'Inventory',
+			path: '/inventory',
+			Icon: InventoryIcon,
+		},
+		{ id: 'sold', label: 'Sold Items', path: '/sold', Icon: AttachMoneyIcon },
+		{
+			id: 'dashboard',
+			label: 'Dashboard',
+			path: '/dashboard',
+			Icon: TrendingUpIcon,
+		},
+		{ id: 'till', label: 'Till', path: '/till', Icon: PointOfSaleIcon },
+		{
+			id: 'settings',
+			label: 'Settings',
+			path: '/settings',
+			Icon: SettingsIcon,
+		},
+	];
 
 	return (
 		<footer className="sticky bottom-0 border-t-2 border-pink-300 border-opacity-50 bg-white py-2">
 			<div className="mx-auto flex w-full max-w-[72rem] justify-between px-4">
-				{tabs.map(({ id, label, Icon }) => {
+				{tabs.map(({ id, label, path, Icon }) => {
 					const isActive = activeTab === id;
 
 					return (
 						<button
 							key={id}
-							type="button"
-							onClick={() => setActiveTab(id)}
+							onClick={() => navigate(path)}
 							className={`flex flex-col items-center rounded-md border-2 px-6 py-2 transition
                 ${
 									isActive
@@ -42,18 +60,15 @@ const Footer = () => {
 						>
 							<Icon
 								fontSize="medium"
-								className={`h-6 w-6 ${
-									isActive ? 'text-pink-500' : 'text-gray-500'
-								}`}
+								className={isActive ? 'text-pink-500' : 'text-gray-500'}
 							/>
-
-							<p
+							<span
 								className={`mt-1 text-sm ${
 									isActive ? 'text-pink-500' : 'text-gray-600'
 								}`}
 							>
 								{label}
-							</p>
+							</span>
 						</button>
 					);
 				})}
