6e1beaa696a990f8b5983adbe816c86589777b8d
Tiffany Gong <76094291+gongt108@users.noreply.github.com>
2026-01-19 13:53:02 -0800

    Create garmentTypes.ts

---
+++ src/constants/garmentTypes.ts
@@ -0,0 +1,10 @@
+import { FaTshirt, FaVest } from 'react-icons/fa';
+import { GiSkirt, GiShorts, GiClothes } from 'react-icons/gi';
+
+export const garmentTypes = [
+  { value: 'shirt', label: 'Shirt', icon: FaTshirt },
+  { value: 'skirt', label: 'Skirt', icon: GiSkirt },
+  { value: 'dress', label: 'Dress', icon: GiClothes },
+  { value: 'shorts', label: 'Shorts', icon: GiShorts },
+  { value: 'jacket', label: 'Jacket', icon: FaVest },
+];
